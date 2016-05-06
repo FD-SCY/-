05d17475b2a223f74f602a7818eabc6d0b69ef79
石纯勇 <shichunyong@shichunyongdemacbook-pro.local>
2016-05-06 14:57:31 +0800

    Initial Commit

---
+++ 九宫格手势密码/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  九宫格手势密码
+//
+//  Created by 石纯勇 on 16/5/6.
+//  Copyright © 2016年 石纯勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 九宫格手势密码/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  九宫格手势密码
+//
+//  Created by 石纯勇 on 16/5/6.
+//  Copyright © 2016年 石纯勇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
